5fc3b0ea3b7db1609ae016aefa983aeb07d7eb1c
nmengel13 <nqm5468@psu.edu>
2022-04-23 18:59:31 -0400

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import { Navbar } from 'react-bootstrap';
+import { Home } from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/home.css';
-import { Container } from 'react-bootstrap/lib/Tab';
+import { Container, Button } from 'react-bootstrap';
+import pies from '../images/pies.jpg';
+import slice from '../images/slice.jpg';
+import muffin from '../images/muffin.jpg';
+import lion from '../images/images.jpeg';
 
 export class Home extends Component {
     render() {
@@ -9,19 +13,40 @@ export class Home extends Component {
         <Container>
         <div class='content'>
             <h1>Welcome to Life of Pie Bakery</h1>
-            <p>We have all your bakery needs! Using the freshest ingredients, we offer various pastries, pies, and muffins for all occasions! Baked daily! <br />Order Today</p>
+            <img src={lion}></img>
+            <p>We have all your bakery needs! Using the freshest ingredients, we offer various pastries, pies, and muffins for all occasions!</p>
         </div>
-        
+
         <div>
-            <div class="bestSeller">
-                <h2>Best Sellers</h2>
-                <p>Our pies are our specialty! Try a slice or the whole pie! We offer various flavors from classic Apple to Cocunut Cream. Find your favorite today!</p>
-            </div>
-            <div class="sale">
-                <h3>On Sale</h3>
-                <p>Our Muffins are currently marked down! Don't worry, they are still just as delicious at a cheaper price! Act Fast, Sale will end soon</p>
-            </div>
+          <img src= {pies}></img>
+          <p>Baked daily!</p>
+          <br />
         </div>
+        <br />
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+          <div class="col">
+        <div class="card" id='piePic'>
+          <div class="card-body">
+            <h5 class="card-title">Best Sellers</h5>
+            <p class="card-text">Our pies are our specialty! Try a slice or the whole pie! We offer various flavors from classic Apple to Cocunut Cream. Find your favorite today!</p>
+          </div>
+          <img src={slice} class="card-img-bottom" alt="..."></img>
+        </div>
+        </div>
+        <div class="card">
+        <div class="col">
+          <div class="card-body">
+            <h5 class="card-title">On Sale</h5>
+            <p class="card-text">Our Muffins are currently marked down! Don't worry, they are still just as delicious at a cheaper price! Act Fast, Sale will end soon</p>
+          </div>
+          <img src={muffin} class="card-img-bottom" alt="..."></img>
+        </div>
+        </div>
+        </div>
+        <br />
+        <button class="btn btn-link">
+            Order Today!
+          </button>
         </Container>
       )
     }
