88ad016e963d742f5c36fcf9fd7f89f04ec9760c
RayRaymond1 <93665923+RayRaymond1@users.noreply.github.com>
2022-04-25 11:04:26 -0400

    some cleanup on the invoice

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -4,13 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../assets/logo.png';
 
 
-var formatter = new Intl.NumberFormat('en-US', { //found out about a numberformatter in JS through stackexchange
+var formatter = new Intl.NumberFormat('en-US', { //found out about a numberformatter through stackexchange
     style: 'currency',
     currency: 'USD',
-
-    // These options are needed to round to whole numbers if that's what you want.
-    //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
 
 let totalAmount = formatter.format(15.00);
