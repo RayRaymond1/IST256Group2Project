88ef4a2d88cb92eb35738a2d93e7b77aa2b4fb6b
nmengel13 <nqm5468@psu.edu>
2022-04-29 13:39:53 -0400

    Added Routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Home } from './components/Home.js';
 import Invoice from './components/Invoice.js'
 import ProductList from './components/productList';
+import Product from './components/product';
+import MyNav from './components/MyNav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <ProductList/>
+    <div className='App'> 
+      <MyNav />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/products' element={<ProductList />} />
+            <Route path='' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/cart' /> //add cart element
+          </Routes>
+        </BrowserRouter>
+    </div>
+
   );
 }
 export default App;
